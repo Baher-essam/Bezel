4dee8ecc1d67fd36debb004ca05943ff45f1714f
Baher-essam <44957599+Baher-essam@users.noreply.github.com>
2020-01-16 23:43:50 +0200

    loading Screen

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 
-
-
-
+//loading screen
+$(document).ready(function(){
+    $("#loading").fadeOut(2500);
+    $(".sk-folding-cube").fadeOut(2500 , function(){
+        $("html").css("overflow" , "auto");
+    })
+  })
 
 //wow.js fire function
 new WOW().init();
